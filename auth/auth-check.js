dc84eb3b5162eb92674490327fb32f1ebcd5e037
Venkateswaran K <k.venkat.1996@gmail.com>
2019-04-30 21:41:33 +0530

    Update auth-check.js

--- auth/auth-check.js
+++ auth/auth-check.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req,res,next)=>{
-    try{
+    try{g
         const token = req.headers.authorization.split(" ")[1];
         console.log("JWT token :",token)
         const isValid = jwt.verify(token,process.env.PRIVATE_KEY);
